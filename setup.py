811957571beba335cf1a97920637b53a47f96d46
Edward Newell <edward.newell@gmail.com>
2017-07-17 13:27:18 -0400

    update setup

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
 	# single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.2.0',
+    version='1.2.1',
 
     description='A queue for python that feels like an iterable and knows when its producers are finished',
     long_description=long_description,
